5bf3b314ab1d5810c5ae2e48791c14cb488278a3
Guilboma <51034418+GuilbomaGames@users.noreply.github.com>
2021-02-15 18:52:27 +0100

    Challenge 001

---
+++ Assets/Scripts/Challenges/001 - 010/001 - ASL/ASL.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASL : MonoBehaviour
+{
+    // Variable for age
+    // Variable for gender
+    // Variable for location
+
+    [SerializeField] private int age;
+    [SerializeField] private char gender;
+    [SerializeField] private string location;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Your age: " + age);
+            Debug.Log("Your gender: " + gender);
+            Debug.Log("Your location: " + location);
+        }
+    }
+}
