ffb60fa6aa4d32dfbd8addd78fc8b88d9fd15f4a
Guilboma <51034418+GuilbomaGames@users.noreply.github.com>
2021-02-16 10:50:36 +0100

    Challenge 006

---
+++ Assets/Scripts/Challenges/001 - 010/006 - Gravity Falls/GravityFalls.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Challenges
+{
+    public class GravityFalls : MonoBehaviour
+    {
+        // handle to the rigidbody
+        // handle to the floor meshRenderer
+        // if I hit the space key, activate the rigidbody gravity and turn the floor meshRenderer red
+        // if the sphere hits the floor, turn the floor meshRenderer blue and destroy me
+
+        private Rigidbody _rigidbody;
+        private MeshRenderer _floorMesh;
+        
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _floorMesh = GameObject.Find("Floor").GetComponent<MeshRenderer>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rigidbody.useGravity = true;
+                _floorMesh.material.color = Color.red;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Floor"))
+            {
+                _floorMesh.material.color = Color.blue;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
